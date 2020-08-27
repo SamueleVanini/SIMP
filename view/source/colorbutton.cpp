2ab5dcd54d09db8331e4ec18858af8e314bcd899
rizzostefano <rstefano99@gmail.com>
2020-08-27 19:19:29 +0200

    alleggerito il codice di canvas, rimosse cose inutili

--- view/source/colorbutton.cpp
+++ view/source/colorbutton.cpp
@@ -5,26 +5,11 @@ ColorButton::ColorButton(QWidget *parent) : QPushButton(parent)
 
 void ColorButton::setAction(QAction *action)
 {
-    // if I've got already an action associated to the button
-    // remove all connections
-    if (actionOwner && actionOwner != action) {
-        disconnect(actionOwner, &QAction::changed,
-                   this, &ColorButton::updateButtonStatusFromAction);
-        disconnect(this, &ColorButton::clicked,
-                   actionOwner, &QAction::trigger);
-    }
-
     // store the action
     actionOwner = action;
 
     // configure the button
     updateButtonStatusFromAction();
-
-    // connect the action and the button
-    // so that when the action is changed the
-    // button is changed too!
-    connect(action, &QAction::changed,
-            this, &ColorButton::updateButtonStatusFromAction);
 
     // connect the button to the slot that forwards the
     // signal to the action
@@ -45,9 +30,6 @@ void ColorButton::updateButtonStatusFromAction()
     if (!actionOwner)
         return;
     setText(actionOwner->text());
-    setStatusTip(actionOwner->statusTip());
-    setToolTip(actionOwner->toolTip());
-    setIcon(actionOwner->icon());
     setEnabled(actionOwner->isEnabled());
     setCheckable(actionOwner->isCheckable());
     setChecked(actionOwner->isChecked());
